3029766d96ce35334ffdd31edc637bfb952d5d0f
Dmitry Sarkisyan <dmitry606@gmail.com>
2016-01-10 18:07:24 +0200

    Initial

---
+++ src/Mvc2Ang_routes_problem/wwwroot/js/site.js
@@ -0,0 +1,42 @@
+﻿
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(['$routeProvider', '$locationProvider',
+	function ($routeProvider, $locationProvider) {
+		$routeProvider
+			//.when('/', {
+			//	templateUrl: 'Index.html',
+			//	controller: 'StartController',
+			//})
+			.when('/games', {
+				templateUrl: 'partials/gameslist.html',
+				controller: 'GameController',
+				controllerAs: 'ctrl'
+			})
+			.when('/games/:gameId', {
+				templateUrl: 'partials/game.html',
+				controller: 'GameController',
+				controllerAs: 'ctrl'
+			});
+
+		$locationProvider.html5Mode(true);
+	}]);
+
+
+myApp.controller("IndexController", ['$route', '$routeParams', '$location',
+  function ($route, $routeParams, $location) {
+		this.$route = $route;
+		this.$location = $location;
+		this.$routeParams = $routeParams;
+		
+		this.doSomething = function () {
+			alert("Something is done!");
+		}
+  }
+]);
+
+myApp.controller("GameController", ['$route', '$routeParams', '$location',
+	function ($route, $routeParams, $location) {
+		this.gameId = $routeParams['gameId'];
+	}
+]);
